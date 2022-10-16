5a96cef559fc91cdf38eec77cd5aa5138bfd6a09
crashtellini <107076865+crashtellini@users.noreply.github.com>
2022-10-15 22:04:13 -0700

    adding boilerplate

---
+++ src/test/java/com/sandsbeach/surfreport/SurfreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandsbeach.surfreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
