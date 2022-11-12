3b7553acae97c240d6de7b0b1b1ae8c52fe47f5f
crashtellini <107076865+crashtellini@users.noreply.github.com>
2022-11-11 17:31:21 -0800

    adding cross origin

--- src/main/java/com/sandsbeach/surfreport/controller/SurfController.java
+++ src/main/java/com/sandsbeach/surfreport/controller/SurfController.java
@@ -5,10 +5,7 @@ import com.sandsbeach.surfreport.service.SurflineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,6 +15,8 @@ public class SurfController {
     @Autowired
     private SurflineService surflineService;
 
+
+    @CrossOrigin
     @GetMapping("/{locationId}")
     public ResponseEntity<SurfLocationReport> getSurfLocationReport(
             @PathVariable("locationId") String locationId) {
